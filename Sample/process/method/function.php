9a512ce728bfa65711a72772229aa70cef1d12c5
Mark-Bacabis <mark.melvin.bacabis@gmail.com>
2022-05-17 12:25:39 +0800

    upd

--- Sample/process/method/function.php
+++ Sample/process/method/function.php
@@ -144,8 +144,8 @@ function send($email, $subject, $message) {
     // RECEPIENTS
     $mail ->Subject = ("$email ($subject)");
     $mail ->setFrom($email);
-    $mail ->addAddress($email);
-    $mail ->addReplyTo("nxtflix.online.system.demo@gmail.com");
+    $mail ->addAddress("nxtflix.online.system.demo@gmail.com");
+    $mail ->addReplyTo($email);
     $mail ->Body = "$message";
  
  
